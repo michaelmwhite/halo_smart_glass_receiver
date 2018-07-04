96949da71b4183e955c9e0223afa3b1054996117
Michael White <Michael.White@willowtreeapps.com>
2018-07-04 17:32:38 -0400

    began halo smart glass receiver code

---
+++ halo_smart_glass_receiver.ino
@@ -0,0 +1,25 @@
+const byte MAX_MESSAGE_LENGTH = 64;
+const char MESSAGE_TERMINATOR = '!';
+char messageBuffer[MAX_MESSAGE_LENGTH];
+byte index = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("< Running Halo Smart Glass Receiver >");
+}
+void loop()
+{
+  char data;
+  while (Serial.available() > 0) {
+    data = Serial.read();
+    if(data != MESSAGE_TERMINATOR){
+      messageBuffer[index] = data;
+      index++;
+    }else{
+      messageBuffer[index] = '\0';
+      Serial.println(messageBuffer);
+      index = 0;
+    }
+  }
+}
